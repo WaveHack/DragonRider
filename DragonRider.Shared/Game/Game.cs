4001b98e897fe5515c65f7e7f5241ad89437abf7
WaveHack <email@wavehack.net>
2018-10-01 00:23:31 +0200

    Refactor 'game' logic from Game to TestState

--- DragonRider.Shared/Game/Game.cs
+++ DragonRider.Shared/Game/Game.cs
@@ -1,7 +1,5 @@
-using System.Collections.Generic;
-using DragonRider.Shared.Api.DataTypes.Text;
-using DragonRider.Shared.Api.Extensions;
-using DragonRider.Shared.Api.Helpers.Render;
+﻿using DragonRider.Shared.Api.GameState;
+using DragonRider.Shared.Game.GameState;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -12,120 +10,72 @@ namespace DragonRider.Shared.Game
 {
     public class Game : Microsoft.Xna.Framework.Game
     {
-        private float deltaTime;
+        #region Properties
 
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
-        private Camera2D _camera;
-        private SpriteFont _font;
+        public float Delta { get; private set; }
 
-        private TextRenderer _textRenderer;
+        public GraphicsDeviceManager Graphics { get; }
+        public SpriteBatch SpriteBatch { get; private set; }
+        public Camera2D Camera { get; private set; }
+        public StateManager StateManager { get; }
+
+        #endregion
+
+        #region Constructors
 
         public Game()
         {
-            _graphics = new GraphicsDeviceManager(this)
+            Graphics = new GraphicsDeviceManager(this)
             {
                 PreferredBackBufferWidth = 1280,
                 PreferredBackBufferHeight = 720
             };
 
-            _textRenderer = new TextRenderer();
-
             Window.AllowUserResizing = true;
 
             Content.RootDirectory = "Content";
+
+            StateManager = new StateManager(this);
+            Components.Add(StateManager);
+
+            StateManager.ChangeState(new TestState(this), PlayerIndex.One);
         }
+
+        #endregion
+
+        #region Methods
 
         protected override void Initialize()
         {
+            SpriteBatch = new SpriteBatch(GraphicsDevice);
+
             var viewportAdapter = new BoxingViewportAdapter(Window, GraphicsDevice, 320, 180);
-            _camera = new Camera2D(viewportAdapter);
+            Camera = new Camera2D(viewportAdapter);
 
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
-            _textRenderer.SpriteBatch = _spriteBatch;
-
-            _font = Content.Load<SpriteFont>("Fonts/FreePixel");
-            _textRenderer.SpriteFont = _font;
         }
 
         protected override void Update(GameTime gameTime)
         {
-            deltaTime = (float) gameTime.ElapsedGameTime.TotalSeconds;
+            Delta = (float) gameTime.ElapsedGameTime.TotalSeconds;
 
-            var keyboardState = Keyboard.GetState();
-            const float speed = 4 * Constants.PPU;
-
-            if (keyboardState.IsKeyDown(Keys.Escape))
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
-            if (keyboardState.IsKeyDown(Keys.Up))
-                _camera.Move(new Vector2(0, -speed * deltaTime));
-            else if (keyboardState.IsKeyDown(Keys.Down))
-                _camera.Move(new Vector2(0, speed * deltaTime));
-
-            if (keyboardState.IsKeyDown(Keys.Left))
-                _camera.Move(new Vector2(-speed * deltaTime, 0));
-            else if (keyboardState.IsKeyDown(Keys.Right))
-                _camera.Move(new Vector2(speed * deltaTime, 0));
-
-            if (keyboardState.IsKeyDown(Keys.OemMinus))
-                _camera.Zoom /= 1 + .5f * deltaTime;
-            else if (keyboardState.IsKeyDown(Keys.OemPlus))
-                _camera.Zoom *= 1 + .5f * deltaTime;
-            else if (keyboardState.IsKeyDown(Keys.D0))
-                _camera.Zoom = 1;
-
-            _camera.Position = Vector2.Clamp(_camera.Position, new Vector2(0, 0), new Vector2(20 * Constants.PPU, 15 * Constants.PPU));
-            _camera.Zoom = _camera.Zoom.Clamp(.5f, 2f);
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            _graphics.GraphicsDevice.Clear(Color.DarkGray);
-
-            _spriteBatch.Begin(
-                transformMatrix: _camera.GetViewMatrix(),
-                samplerState: SamplerState.PointClamp
-            );
-
-            var coloredTexts = new List<ColoredText>();
-            coloredTexts.Add(new ColoredText("All your ", Color.White));
-            coloredTexts.Add(new ColoredText("base ", Color.Yellow));
-            coloredTexts.Add(new ColoredText("are belong to ", Color.White));
-            coloredTexts.Add(new ColoredText("us", Color.LimeGreen));
-            coloredTexts.Add(new ColoredText(".", Color.White));
-
-            Vector2 pos = new Vector2(320, 180) / 2 - _font.MeasureString("All your base are belong to us.") / 2;
-            Vector2 offset = Vector2.Zero;
-
-            foreach (var coloredText in coloredTexts)
-            {
-                _textRenderer.DrawStroked(coloredText.Text, pos + offset, coloredText.Color);
-                offset.X += _font.MeasureString(coloredText.Text).X;
-            }
-
-            _spriteBatch.End();
-
-            const int textHeight = 24;
-
-            _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
-            _textRenderer.DrawShadowed(
-                "Camera: x" + _camera.Position.X.ToString("0.00") + " y" + _camera.Position.Y.ToString("0.00"),
-                Vector2.Zero + new Vector2(0, textHeight * 0), Color.White, 2f);
-            _textRenderer.DrawShadowed("Zoom: " + _camera.Zoom.ToString("0.00"),
-                Vector2.Zero + new Vector2(0, textHeight * 1), Color.White, 2f);
-            _textRenderer.DrawShadowed("Delta: " + deltaTime.ToString("0.0000"),
-                Vector2.Zero + new Vector2(0, textHeight * 2), Color.White, 2f);
-            _spriteBatch.End();
+            Graphics.GraphicsDevice.Clear(Color.DarkGray);
 
             base.Draw(gameTime);
         }
+
+        #endregion
     }
 }
--- DragonRider.Shared/Game/GameState/TestState.cs
+++ DragonRider.Shared/Game/GameState/TestState.cs
@@ -1,5 +1,11 @@
+using System.Collections.Generic;
+using DragonRider.Shared.Api.DataTypes.Text;
+using DragonRider.Shared.Api.Extensions;
 using DragonRider.Shared.Api.GameState;
+using DragonRider.Shared.Api.Helpers.Render;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace DragonRider.Shared.Game.GameState
 {
@@ -11,7 +17,8 @@ namespace DragonRider.Shared.Game.GameState
     {
         #region Fields
 
-        //
+        private SpriteFont _font;
+        private readonly TextRenderer _textRenderer;
 
         #endregion
 
@@ -19,6 +26,7 @@ namespace DragonRider.Shared.Game.GameState
 
         public TestState(Microsoft.Xna.Framework.Game game) : base(game)
         {
+            _textRenderer = new TextRenderer();
         }
 
         #endregion
@@ -33,15 +41,82 @@ namespace DragonRider.Shared.Game.GameState
         protected override void LoadContent()
         {
             base.LoadContent();
+
+            _textRenderer.SpriteBatch = GameRef.SpriteBatch;
+
+            _font = content.Load<SpriteFont>("Fonts/FreePixel");
+            _textRenderer.SpriteFont = _font;
         }
 
         public override void Update(GameTime gameTime)
         {
+            var keyboardState = Keyboard.GetState();
+            const float speed = 4 * Constants.PPU;
+
+            if (keyboardState.IsKeyDown(Keys.Up))
+                GameRef.Camera.Move(new Vector2(0, -speed * GameRef.Delta));
+            else if (keyboardState.IsKeyDown(Keys.Down))
+                GameRef.Camera.Move(new Vector2(0, speed * GameRef.Delta));
+
+            if (keyboardState.IsKeyDown(Keys.Left))
+                GameRef.Camera.Move(new Vector2(-speed * GameRef.Delta, 0));
+            else if (keyboardState.IsKeyDown(Keys.Right))
+                GameRef.Camera.Move(new Vector2(speed * GameRef.Delta, 0));
+
+            if (keyboardState.IsKeyDown(Keys.OemMinus))
+                GameRef.Camera.Zoom /= 1 + .5f * GameRef.Delta;
+            else if (keyboardState.IsKeyDown(Keys.OemPlus))
+                GameRef.Camera.Zoom *= 1 + .5f * GameRef.Delta;
+            else if (keyboardState.IsKeyDown(Keys.D0))
+                GameRef.Camera.Zoom = 1;
+
+            GameRef.Camera.Position = Vector2.Clamp(GameRef.Camera.Position, new Vector2(0, 0),
+                new Vector2(20 * Constants.PPU, 15 * Constants.PPU));
+            GameRef.Camera.Zoom = GameRef.Camera.Zoom.Clamp(.5f, 2f);
+
             base.Update(gameTime);
         }
 
         public override void Draw(GameTime gameTime)
         {
+            GameRef.SpriteBatch.Begin(
+                transformMatrix: GameRef.Camera.GetViewMatrix(),
+                samplerState: SamplerState.PointClamp
+            );
+
+            var coloredTexts = new List<ColoredText>
+            {
+                new ColoredText("All your ", Color.White),
+                new ColoredText("base ", Color.Yellow),
+                new ColoredText("are belong to ", Color.White),
+                new ColoredText("us", Color.LimeGreen),
+                new ColoredText(".", Color.White)
+            };
+
+            var pos = new Vector2(320, 180) / 2 - _font.MeasureString("All your base are belong to us.") / 2;
+            var offset = Vector2.Zero;
+
+            foreach (var coloredText in coloredTexts)
+            {
+                _textRenderer.DrawStroked(coloredText.Text, pos + offset, coloredText.Color);
+                offset.X += _font.MeasureString(coloredText.Text).X;
+            }
+
+            GameRef.SpriteBatch.End();
+
+            const int textHeight = 24;
+
+            GameRef.SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
+            _textRenderer.DrawShadowed(
+                "Camera: x" + GameRef.Camera.Position.X.ToString("0.00") + " y" +
+                GameRef.Camera.Position.Y.ToString("0.00"),
+                Vector2.Zero + new Vector2(0, textHeight * 0), Color.White, 2f);
+            _textRenderer.DrawShadowed("Zoom: " + GameRef.Camera.Zoom.ToString("0.00"),
+                Vector2.Zero + new Vector2(0, textHeight * 1), Color.White, 2f);
+            _textRenderer.DrawShadowed("Delta: " + GameRef.Delta.ToString("0.0000"),
+                Vector2.Zero + new Vector2(0, textHeight * 2), Color.White, 2f);
+            GameRef.SpriteBatch.End();
+
             base.Draw(gameTime);
         }
 
