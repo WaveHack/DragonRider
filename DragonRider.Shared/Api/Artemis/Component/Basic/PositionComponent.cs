ad8e7d01b915d6178fe02615fe697e8a625f89c8
WaveHack <email@wavehack.net>
2018-10-03 15:26:40 +0200

    Add Position component

---
+++ DragonRider.Shared/Api/Artemis/Component/Basic/PositionComponent.cs
@@ -0,0 +1,9 @@
+using Microsoft.Xna.Framework;
+
+namespace DragonRider.Shared.Api.Artemis.Component.Basic
+{
+    public class PositionComponent
+    {
+        public Vector2 Position { get; set; }
+    }
+}
